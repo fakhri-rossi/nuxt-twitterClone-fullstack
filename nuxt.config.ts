1b36dac3566431822da2d58b291f381165937bcd
Fakhri Rossi <rossifakhri@gmail.com>
2024-07-25 12:27:53 +0700

    Fixed bugs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@prisma/nuxt", "nuxt-headlessui", '@nuxtjs/device'],
-
+  modules: ["nuxt-headlessui", "@nuxtjs/device", "@nuxt/ui", "@prisma/nuxt"],
+  //  '@nuxtjs/device'
   // build: {
   //   transpile: []
   // },
